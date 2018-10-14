1ce1ec09af5244bfb8d9dcd377578dde889c2d75
Mike Schinkel <mike@newclarity.net>
2018-10-14 18:52:31 -0400

    Add DB_Ops::reset_auto_increment() and ::set_auto_increment()

--- includes/DB_Ops.php
+++ includes/DB_Ops.php
@@ -25,4 +25,38 @@ class DB_Ops {
 		$wpdb->query( "TRUNCATE TABLE {$table}" );
 	}
 
+	/**
+	 * @param string $table
+	 */
+	static function reset_auto_increment( $table ) {
+		return self::set_auto_increment( $table, 1 );
+	}
+
+	/**
+	 * @param string $table
+	 * @param int $next_id
+	 * @return bool Success (true) or failure (false)
+	 */
+	static function set_auto_increment( $table, $next_id ) {
+		do {
+			$success = false;
+			global $wpdb;
+			if ( ! isset( $wpdb->{$table} ) ) {
+				self::$_last_error = new Error( sprintf( 'No property named [%s] in \$wpdb.', $table ) );
+				break;
+			}
+			$table = $wpdb->{$table};
+			if ( ! is_string( $table ) ) {
+				self::$_last_error = new Error( sprintf( 'The \$wpdb property named [%s] is not a string.', $table ) );
+				break;
+			}
+			$wpdb->query( $wpdb->prepare(
+				"ALTER TABLE {$table} AUTO_INCREMENT=%d",
+				$next_id
+			));
+			$success = true;
+		} while ( false );
+		return $success;
+	}
+
 }
